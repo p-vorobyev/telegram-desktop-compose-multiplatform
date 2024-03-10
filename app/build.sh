e1dec08f3a438a4b8680538eaafddf0eb710dbde
Pavel Vorobyev <p.vorobyev@inbox.ru>
2024-03-10 16:55:53 +0300

    deleted bash scripts for building and starting app

--- app/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./gradlew -p ../backend bootJar && ./gradlew -p ../desktop packageUberJarForCurrentOS
-
-rm -rf jars && mkdir jars
-
-cp ../backend/build/libs/backend*.jar ./jars/backend.jar
-
-cp ../desktop/build/compose/jars/TelegramComposeMultiplatform*.jar ./jars/TelegramComposeMultiplatform.jar
