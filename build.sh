9f326e090ced16051d61cb4d454999cf937e62a7
Pavel Vorobyev <p.vorobyev@inbox.ru>
2024-10-13 22:37:40 +0300

    [desktop] downloading libs with a build script

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+cd desktop/resources || exit
+curl -LJO 'https://github.com/p-vorobyev/spring-boot-starter-telegram/releases/download/1.14.0/libs.zip'
+unzip -q libs.zip
+cp libs/linux_arm64/*.so linux-arm64
+cp libs/linux_x64/*.so linux-x64
+cp libs/macos_silicon/*.dylib macos-arm64
+cp libs/macos_x64/*.dylib macos-x64
+cp libs/windows_x64/*.dll windows-x64
+rm libs.zip && rm -rf libs/
+cd ../..
+
 ./gradlew clean
 ./gradlew :backend:bootJar
 ./gradlew :desktop:copyTelegramBackend
