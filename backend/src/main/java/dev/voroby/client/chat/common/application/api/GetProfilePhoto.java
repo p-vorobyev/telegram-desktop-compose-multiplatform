455f951f63ab4267856958fbe94ca5de5814525c
Pavel Vorobyev <p.vorobyev@inbox.ru>
2025-02-09 17:41:10 +0300

    [backend] deprecated api removed & refactoring

--- backend/src/main/java/dev/voroby/client/chat/common/application/api/GetProfilePhoto.java
+++ backend/src/main/java/dev/voroby/client/chat/common/application/api/GetProfilePhoto.java
@@ -1,15 +1,17 @@
 package dev.voroby.client.chat.common.application.api;
 
-import dev.voroby.client.util.Utils;
 import dev.voroby.client.cache.Caches;
-import org.drinkless.tdlib.TdApi;
+import dev.voroby.client.util.Utils;
 import dev.voroby.springframework.telegram.client.TelegramClient;
+import org.drinkless.tdlib.TdApi;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component
 public class GetProfilePhoto implements Function<Long, String> {
+
+    private static final String EMPTY_RESULT = "";
 
     private final TelegramClient telegramClient;
 
@@ -27,17 +29,25 @@ public class GetProfilePhoto implements Function<Long, String> {
                 TdApi.ProfilePhoto profilePhoto = user.profilePhoto;
                 if (profilePhoto != null) {
                     TdApi.File photoFile = profilePhoto.small;
-                    //get consistent file info
-                    photoFile = telegramClient.sendSync(new TdApi.GetFile(photoFile.id));
-                    if (photoFile.local.isDownloadingCompleted) {
-                        String profilePhotoEncoded = Utils.fileBase64Encode(photoFile.local.path);
-                        Caches.userIdToProfilePhotoCache.put(userId, profilePhotoEncoded);
-                        return profilePhotoEncoded;
-                    }
+                    return getProfilePhotoEncodedOrEmpty(userId, photoFile);
                 }
             }
-            return "";
+            return EMPTY_RESULT;
         }
+    }
+
+    private String getProfilePhotoEncodedOrEmpty(long userId, TdApi.File photoFile) {
+        //get consistent file info
+        var optionalFile = telegramClient.send(new TdApi.GetFile(photoFile.id)).getObject();
+        if (optionalFile.isPresent()) {
+            photoFile = optionalFile.get();
+            if (photoFile.local.isDownloadingCompleted) {
+                var profilePhotoEncodedOrEmpty = Utils.fileBase64Encode(photoFile.local.path);
+                Caches.userIdToProfilePhotoCache.put(userId, profilePhotoEncodedOrEmpty);
+                return profilePhotoEncodedOrEmpty;
+            }
+        }
+        return EMPTY_RESULT;
     }
 
 }
--- backend/src/main/java/dev/voroby/client/chat/markAsRead/application/api/MarkMessagesAsRead.java
+++ backend/src/main/java/dev/voroby/client/chat/markAsRead/application/api/MarkMessagesAsRead.java
@@ -6,6 +6,7 @@ import dev.voroby.springframework.telegram.client.TelegramClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
 @Component @Slf4j
@@ -19,32 +20,48 @@ public class MarkMessagesAsRead implements Consumer<Long> {
 
     @Override
     synchronized public void accept(Long chatId) {
-        telegramClient.sendSync(new TdApi.OpenChat(chatId));
+        telegramClient.send(new TdApi.OpenChat(chatId)).onSuccess(ok -> markAsRead(chatId));
+    }
+
+    private void markAsRead(long chatId) {
         try {
             long lastReadInboxMessageId;
-            long nextFromMessageId = 0;
+            long fromMessageId = 0;
             do {
-                TdApi.Chat chat = telegramClient.sendSync(new TdApi.GetChat(chatId));
-                lastReadInboxMessageId = chat.lastReadInboxMessageId;
-                var getChatHistory = new TdApi.GetChatHistory(chatId, nextFromMessageId, 0, 100, true);
-                TdApi.Messages messages = telegramClient.sendSync(getChatHistory);
-                TdApi.Message[] msgs = messages.messages;
-                if (msgs.length == 0) break;
-                long[] ids = new long[msgs.length];
-                for (int i = 0; i < msgs.length; i++) {
-                    ids[i] = msgs[i].id;
-                }
-                var viewMessages = new TdApi.ViewMessages(chatId, ids, null, false);
-                telegramClient.sendSync(viewMessages);
-                nextFromMessageId = ids[ids.length - 1];
-            } while (nextFromMessageId != lastReadInboxMessageId);
+                Optional<TdApi.Chat> chatOptional = telegramClient.send(new TdApi.GetChat(chatId)).getObject();
+                if (chatOptional.isPresent()) {
+                    TdApi.Chat chat = chatOptional.get();
+                    lastReadInboxMessageId = chat.lastReadInboxMessageId;
+                    var getChatHistory = new TdApi.GetChatHistory(chatId, fromMessageId, 0, 100, true);
+                    Optional<TdApi.Messages> messagesOptional = telegramClient.send(getChatHistory).getObject();
+                    if (messagesOptional.isPresent()) {
+                        TdApi.Messages messages = messagesOptional.get();
+                        TdApi.Message[] messagesArray = messages.messages;
+                        if (messagesArray.length == 0) break;
+                        fromMessageId = viewMessagesAndGetFromMessageId(chatId, messagesArray);
+                    }
+                } else break;
+            } while (fromMessageId != lastReadInboxMessageId);
             log.info("Mark messages as read: [chatId: {}]", chatId);
         } finally {
-            // closing only if chat is not opened on frontend
-            if (!Caches.openedChat.get().equals(chatId)) {
-                telegramClient.sendAsync(new TdApi.CloseChat(chatId));
-            }
+            closeChat(chatId);
         }
     }
 
+    private long viewMessagesAndGetFromMessageId(long chatId, TdApi.Message[] messages) {
+        long[] ids = new long[messages.length];
+        for (int i = 0; i < messages.length; i++) {
+            ids[i] = messages[i].id;
+        }
+        var viewMessages = new TdApi.ViewMessages(chatId, ids, null, false);
+        telegramClient.send(viewMessages);
+        return ids[ids.length - 1];
+    }
+
+    private void closeChat(long chatId) {
+        // closing only if chat is not opened on frontend
+        if (!Caches.openedChat.get().equals(chatId)) {
+            telegramClient.sendAsync(new TdApi.CloseChat(chatId));
+        }
+    }
 }
