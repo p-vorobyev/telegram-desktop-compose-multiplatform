59ee7e346061e0e2f29e2e2f227f644c8fffd8f8
Pavel Vorobyev <p.vorobyev@inbox.ru>
2026-01-08 19:12:48 +0300

    build script updated

--- build.sh
+++ build.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-cd desktop/resources || exit
-curl -LJO 'https://github.com/p-vorobyev/spring-boot-starter-telegram/releases/download/1.18.0/libs.zip'
-unzip -q libs.zip
-cp libs/centos_stream9_arm64/*.so linux-arm64
-cp libs/centos_stream9_x64/*.so linux-x64
-cp libs/macos_silicon/*.dylib macos-arm64
-cp libs/macos_x64/*.dylib macos-x64
-cp libs/windows_x64/*.dll windows-x64
-rm libs.zip && rm -rf libs/
-cd ../..
+echo "🧹 Cleaning and building backend..."
+./gradlew clean :backend:bootJar
 
-./gradlew clean
-./gradlew :backend:bootJar
-./gradlew :desktop:copyTelegramBackend
-./gradlew :desktop:createDistributable
+echo "📦 Preparing desktop distribution..."
+./gradlew :desktop:copyTelegramBackend :desktop:createDistributable
+
+echo "✅ Build completed successfully."
